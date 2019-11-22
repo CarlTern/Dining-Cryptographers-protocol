b6e30c36b074414e06457cbaf5a4f118d5fe85ee
Diocles1 <vi1221pe-s@student.lu.se>
2019-11-22 12:58:39 +0100

    minor changes

--- DC-protocol.py
+++ DC-protocol.py
@@ -1,18 +1,20 @@
 if __name__ == "__main__":
-    secretAice = 'D75C'
+    secretAlice = 'D75C'
     dataAlice = 'C568'
     secretBob = 'EE87'
     dataBob = 'FCB3'
     userMessage = '4674'
-    b = 1
+    b = 0
 
     # This is our data to broadcast.
     if(b == 0):
-        dataUser = hex(int('0000', 16) ^ int(secretBob, 16) ^ int(secretAice, 16))
+        dataUser = hex(int('0000', 16) ^ int(secretBob, 16) ^ int(secretAlice, 16))
         message = hex(int(dataAlice, 16) ^ int(dataBob, 16) ^ int(dataUser, 16))
-        print('b=0:\t', dataUser[2:] + message[2:])
+        if(message == '0x0'):
+            message = '0000'
+        print('b=0:\t', dataUser[2:] + message)
 
     elif(b == 1):
-        dataUser = hex(int(userMessage, 16) ^ int(secretAice, 16) ^ int(secretBob, 16))
+        dataUser = hex(int(userMessage, 16) ^ int(secretAlice, 16) ^ int(secretBob, 16))
         message = hex(int(dataAlice, 16) ^ int(dataBob, 16) ^ int(dataUser, 16))
-        print('b=1:\t', dataUser)
+        print('b=1:\t', dataUser[2:])
